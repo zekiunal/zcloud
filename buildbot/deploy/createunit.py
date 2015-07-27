58502dd454d7e2cd46dcfe934dae3156fd686d62
Evren Kutar <furiston@gmail.com>
2015-07-27 17:16:38 +0300

    slave host and port from env and unit file to trigger

--- buildbot/deploy/createunit.py
+++ buildbot/deploy/createunit.py
@@ -1,27 +1,38 @@
 import requests
 import json
-import datetime
+import time
 import sys
+import re
 
 url = 'http://zx-control-center.c.zetaops-academic-erp.internal:49153/fleet/v1/units'
+unit_url = 'https://raw.githubusercontent.com/zetaops/zcloud/master/units/'
 headers={'content-type':'application/json'}
 
-def getUnits():
-	return requests.get(url).json()['units']
+def incrementUnitName(unitname):
+	units = [u['name'] for u in requests.get(url).json()['units']]
+	for unit in units:
+		if unitname.split('@')[0] in unit:
+			name = unit
+	return '@'.join([unitname.split('@')[0], str(int(name.split('@')[1][0])+1) + '.service'])
 
-def getCurrentServiceOptions(unit):
-	for i in getUnits():
-		if unit in i['name']:
-			return i['options']
+def createServiceOptions(unitname):
+	r = requests.get(unit_url+unitname)
+	lines=r.content.split('\n')
+	section = ''
+	options = []
+	for line in lines:
+		if '[' in line:
+			section = re.search('\[(.*)\]', line).group(1)
+		options.append({'section':section, 'name': line.split('=')[0], 'value': '='.join(line.split('=')[1:])}) if '=' in line else True
+	return options
 
-def createUnit(unit):
-	print unit, unit[-1]
-	if unit[-1] != '@':
+def createUnit(unitname):
+	if unitname[-1] != '@':
 		raise Exception('unitname must end with @')
 	newunit = {}
-	newunit['name'] = unit + datetime.date.today().isoformat() + '.service'
+	newunit['name'] = incrementUnitName(unitname)
 	newunit['desiredState'] = 'loaded'
-	newunit['options'] = getCurrentServiceOptions(unit)
+	newunit['options'] = createServiceOptions(unitname)
 	unitcreate = requests.put(url+'/'+newunit['name'], data=json.dumps(newunit), headers=headers)
 	return unitcreate.content
 
