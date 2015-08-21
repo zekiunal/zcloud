c6870cf94315fc2579fa8655ec8efdebdf449ecb
dyrnade <gurescicem@gmail.com>
2015-08-21 13:44:15 +0300

    made changes to auto-install ulakbus

--- containers/ulakbus-dockerfile/app.sh
+++ containers/ulakbus-dockerfile/app.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
-
 # create app directory and clone zaerp master
 mkdir /app
 cd /app
+
+### Clone ulakbus from github ###
 git clone https://github.com/zetaops/ulakbus.git
 
-cd /app/ulakbus
-pip install -r requirements.txt
-ln -s /app/ulakbus/ulakbus /usr/local/lib/python2.7/dist-packages/
+### Run setup.py ###
+cd /app/ulakbus/
+python setup.py install
 
-
-# add zaerp user and set /app as home
-/usr/sbin/useradd --home-dir /app --shell /bin/bash --comment 'zaerp operations' ulakbus
-chown -R ulakbus:ulakbus /app
-cd /app/ulakbus/ulakbus
-
+### Run Server ###
+cd /app/ulakbus/ulakbus/
 python runserver.py
