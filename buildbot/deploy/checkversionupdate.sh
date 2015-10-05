1c4546e61ffe14a79051d40bd6f3d91694414570
Evren Kutar <furiston@gmail.com>
2015-10-05 17:16:53 +0300

    fix for get latest tag of repo

--- buildbot/deploy/checkversionupdate.sh
+++ buildbot/deploy/checkversionupdate.sh
@@ -3,14 +3,14 @@
 # git diff-tree --no-commit-id --name-only -r (git rev-parse HEAD)
 
 # checkout to latest tag
-# git checkout (git describe --tags)
+# git checkout (git describe --abbrev=0 --tags)
 
 
 FILES=$(git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD))
 
 # check if version file in latest commit
 if echo $FILES | grep -q "VERSION"; then
-    git checkout $(git describe --tags);
+    git checkout $(git describe --abbrev=0 --tags);
     echo 1;
 else
 	echo 0;
