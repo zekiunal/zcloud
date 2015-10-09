b12563a0c819e6fdf63e9854aedc72b3ea318885
Ali Riza Keles <ali.r.keles@gmail.com>
2015-10-09 10:46:47 +0300

    ulakbus nightly container added

---
+++ containers/ulakbus-nightly-dockerfile/app.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# create app directory and clone zaerp master
+mkdir /app
+cd /app
+
+### Clone ulakbus from github ###
+git clone https://github.com/zetaops/ulakbus.git
+
+### Run setup.py ###
+cd /app/ulakbus/
+git checkout $(git describe --abbrev=0 --tags)
+python setup.py install
+
+### Run Server ###
+#cd /app/ulakbus/ulakbus/
+#python runserver.py
+
+cd /app/ulakbus/gunicorn
+gunicorn server:app -c gunicorn_config.py
